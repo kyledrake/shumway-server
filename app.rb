17f783708aa699224a29bfdc9189a20bc47cd3d0
Kyle Drake <kyle@kyledrake.net>
2016-04-25 20:41:49 -0700

    spit out params

--- app.rb
+++ app.rb
@@ -1,3 +1,10 @@
+$redis = Redis.new(url: ENV["REDIS_URL"])
+
+require 'time'
+
 get '/?' do
-  'Hello World'
+  if params[:timesec] && params[:chipid]
+    puts "Time: #{Time.at(params[:timesec].to_f)} (#{params[:timesec]}) ChipID: #{params[:chipid]}"
+  end
+  'ok'
 end
